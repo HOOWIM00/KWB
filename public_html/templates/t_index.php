89a3b91bd067c1a33ef8675361bd14b49f3d99d7
HOOWIM00 <wim.hooyberghs@sibelco.com>
2025-10-25 13:36:23 +0200

    start git

---
+++ public_html/templates/t_index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">  
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>RAAK Achterbos - www.raakachterbos.be</title>
+    </head>
+    <body>
+        <?php
+        // php info
+        phpinfo();
+        // Welkom op de website
+        echo "Hallo, welkom bij RAAK Achterbos!";
+        ?>
+    </body>
+</html>
