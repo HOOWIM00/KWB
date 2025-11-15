5e1db41d5db819e36f6553d90cf479f54472226f
Hooy80 <wim.hooyberghs80@gmail.com>
2025-11-15 13:55:19 +0100

    setup react raak

--- public_html/includes/main.php
+++
@@ -1,5 +0,0 @@
-<main class="iframe-container">
-  <iframe src="iframes/nav_left.php" name="nav_left" class="iframe-left"></iframe>
-  <iframe src="iframes/home.php" name="main" class="iframe-main"></iframe>
-  <iframe src="iframes/nav_right.php" name="nav_right" class="iframe-right"></iframe>
-</main>
